e05b801301ad0b4d8073aacb980bdb98155bb335
Dustin Hennessy <Hennessydustin@gmail.com>
2015-06-25 09:54:42 -0400

    Initial Commit

---
+++ TransitEasy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TransitEasy
+//
+//  Created by Dustin Hennessy on 6/25/15.
+//  Copyright (c) 2015 DustinHennessy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TransitEasy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TransitEasy
+//
+//  Created by Dustin Hennessy on 6/25/15.
+//  Copyright (c) 2015 DustinHennessy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
